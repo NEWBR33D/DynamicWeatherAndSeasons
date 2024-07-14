8f52cae4f65be4f496b6d7414d8d5ea72799eb13
PreyToLive <94263500+NEWBR33D@users.noreply.github.com>
2024-07-13 20:49:31 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,40 @@
+# **PreyToLive-DynamicWeatherAndSeasons v1.0.0**
+
+Developed by: [NEWBR33D](https://github.com/NEWBR33D) (aka [PreyToLive](https://hub.sp-tarkov.com/user/24548-preytolive/))
+
+[DynamicWeatherAndSeasons](https://github.com/NEWBR33D/DynamicWeatherAndSeasons/) is a mod that allows control over the weather and seasons for SPTarkov.
+
+#### ***If you would like to support me and my work you can donate to me [here](https://ko-fi.com/preytolive). Thank you!***
+
+## **Installation Instructions:**
+1. Begin by downloading the "PreyToLive-DynamicWeatherAndSeasons-v1.0.0.zip" file from the provided link or the Github repository.
+2. Extract the contents of the downloaded archive. You should now have a folder named "PreyToLive-DynamicWeatherAndSeasons" containing the mod files.
+3. Navigate to the location of your SPTarkov folder on your computer.
+4. Inside the SPTarkov folder, find the "user/mods/" directory.
+5. Place the extracted folder containing the mod files into the 'mods' folder within your SPTarkov folder.
+
+## **Setup Guide:**
+1. Start by opening the config file located in the mods config folder.
+2. Set a value for "monthDurationInRaids". Each raid counts as one day in a month.
+3. Set a season (summer, autumn, winter, spring) for each month. Example given below.
+```
+    "monthDurationInRaids": 15,
+    "months": {
+        "january": "winter",
+        "february": "spring",
+        "march": "spring",
+        "april": "spring",
+        "may": "summer",
+        "june": "summer",
+        "july": "summer",
+        "august": "summer",
+        "september": "autumn",
+        "october": "autumn",
+        "november": "winter",
+        "december": "winter"
+    },
+
+    e.g. Lets say your profile starts in July. The season generated in-raid will be Summer. To transition from Summer to Autumn you will need to have had a total of 30 raids to get through all of July (15) and August (15). Your 31st raid will transition to Autumn. To get to Winter you will need another 30 raids total and then another 45 after that to get to Spring.
+```
+4. Set the weather type for each season. Weather weights and min/max values can be found in "db/weather.json". (Do NOT edit or change the weather.json unless you know what you are doing.)
+5. All DynamicWeatherAndSeasons profile information generated before each raid (for reference) can be found in the "db/profile.json". (Do NOT edit or change the profile.json unless you know what you are doing.)
---
+++ src/DWSMain.ts
@@ -0,0 +1,291 @@
+/*
+ * DynamicWeatherAndSeasons v1.0.0
+ * MIT License
+ * Copyright (c) 2024 PreyToLive
+ */
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/brace-style */
+
+import { DependencyContainer } from "tsyringe";
+import { PreSptModLoader } from "@spt/loaders/PreSptModLoader";
+import { ConfigTypes } from "@spt/models/enums/ConfigTypes";
+import { IPostSptLoadMod } from "@spt/models/external/IPostSptLoadMod";
+import { IWeatherConfig } from "@spt/models/spt/config/IWeatherConfig";
+import { ILogger } from "@spt/models/spt/utils/ILogger";
+import { ConfigServer } from "@spt/servers/ConfigServer";
+import { StaticRouterModService } from "@spt/services/mod/staticRouter/StaticRouterModService";
+import { LoggerTypes } from "../enums/DWSEnumLogger";
+import * as path from "path";
+import * as fs from "fs";
+import pkg from "../package.json";
+
+class DWSMain implements IPostSptLoadMod {
+    private logger: ILogger;
+
+    public preSptLoad(container: DependencyContainer): void {
+        this.logger = container.resolve<ILogger>("WinstonLogger");
+
+        const preSptModLoader = container.resolve<PreSptModLoader>("PreSptModLoader");
+        const staticRouterModService: StaticRouterModService = container.resolve<StaticRouterModService>("StaticRouterModService");
+        
+        const modName = `${pkg.author}-${pkg.name}-v${pkg.version}`;
+        const modPath = preSptModLoader.getModPath(path.basename(path.dirname(__dirname.split('/').pop())));
+
+        staticRouterModService.registerStaticRouter(
+            `[${pkg.name}] /client/items`, 
+            [
+                {
+                    url: "/client/items",
+                    action: (url: string, info: any, sessionID: string, output: string): any => {
+                        try {
+                            const sptConfigsWeather: IWeatherConfig = container.resolve<ConfigServer>("ConfigServer").getConfig<IWeatherConfig>(ConfigTypes.WEATHER);
+
+                            const dbConfig = require("../config/config.json");
+                            const dbProfile = require("../db/profile.json");
+                            const dbWeather = require("../db/weather.json");
+
+                            if (dbConfig.modEnabled) {
+                                
+                                // START UPDATE PROFILE
+
+                                let profileDay = dbProfile.date.day;
+                                let profileMonth = dbProfile.date.month;
+                                let profileYear = dbProfile.date.year;
+                                let profileSeason = dbProfile.season;
+
+                                const monthDurationInRaids = dbConfig.monthDurationInRaids;
+                                const monthIndices = {
+                                    january: 1,
+                                    february: 2,
+                                    march: 3,
+                                    april: 4,
+                                    may: 5,
+                                    june: 6,
+                                    july: 7,
+                                    august: 8,
+                                    september: 9,
+                                    october: 10,
+                                    november: 11,
+                                    december: 12
+                                }
+
+                                profileDay++;
+                                if (profileDay > monthDurationInRaids) {
+                                    const monthIndex = monthIndices[profileMonth.toLowerCase()] || 1;
+                                    const nextMonthIndex = monthIndex === 12 ? 1 : monthIndex + 1;
+
+                                    profileDay = 1;
+                                    profileMonth = Object.keys(monthIndices).find(key => monthIndices[key] === nextMonthIndex) || "january";
+                                    
+                                    if (nextMonthIndex === 1) {
+                                        profileYear++;
+                                    }
+                                }
+                                profileSeason = dbConfig.months[profileMonth];
+
+                                // START UPDATE WEATHER AND SEASONS
+
+                                const weatherSelections: Record<string, number> = {};
+                                for (const [key, value] of Object.entries(dbConfig.seasons[profileSeason].weather)) {
+                                    if (Array.isArray(value)) {
+                                        weatherSelections[key] = value[Math.floor(Math.random() * value.length)];
+                                    }
+                                }
+                                const { clouds, rain, wind, fog, temp, pressure } = weatherSelections;
+
+                                // CLOUDS
+                                const cloudsProfileDesc: string[] = [
+                                    "clear sky",
+                                    "partly cloudy",
+                                    "mostly cloudy",
+                                    "overcast with scattered clouds",
+                                    "completely overcast with thick clouds"
+                                ];
+                                const cloudsProfile = cloudsProfileDesc[clouds];
+                                sptConfigsWeather.weather.clouds = dbWeather.clouds[clouds];
+
+                                // RAIN
+                                const rainProfileDesc: string[] = [
+                                    "no rain",
+                                    "light rain",
+                                    "moderate rain",
+                                    "heavy rain",
+                                    "intense rain"
+                                ];
+                                let rainProfile: string;
+                                if (clouds === 0) {
+                                    rainProfile = rainProfileDesc[0];
+                                    sptConfigsWeather.weather.rain = dbWeather.rain[0];
+                                    sptConfigsWeather.weather.rainIntensity = dbWeather.rainIntensity[0];
+                                } else {
+                                    rainProfile = rainProfileDesc[rain];
+                                    sptConfigsWeather.weather.rain = dbWeather.rain[rain];
+                                    sptConfigsWeather.weather.rainIntensity = dbWeather.rainIntensity[rain];
+                                }
+
+                                // WIND
+                                const windProfileDesc: string[] = [
+                                    "calm",
+                                    "gentle breeze",
+                                    "moderate breeze",
+                                    "strong wind",
+                                    "stormy wind"
+                                ];
+                                const windProfile = windProfileDesc[wind];
+                                sptConfigsWeather.weather.windSpeed = dbWeather.windSpeed[wind];
+                                sptConfigsWeather.weather.windGustiness = dbWeather.windGustiness[wind];
+
+                                const seasonIndices: Record<string, number> = {
+                                    "spring": 1,
+                                    "summer": 2,
+                                    "autumn": 3,
+                                    "winter": 4
+                                };
+                                const seasonIndex = seasonIndices[profileSeason] || 0;
+                                sptConfigsWeather.weather.windDirection = dbWeather.windDirection[seasonIndex];
+
+                                // FOG
+                                const fogProfileDesc: string[] = [
+                                    "no fog",
+                                    "light fog",
+                                    "moderate fog",
+                                    "dense fog",
+                                    "thick fog"
+                                ];
+                                const fogProfile = fogProfileDesc[fog];
+                                sptConfigsWeather.weather.fog = dbWeather.fog[fog];
+
+                                // TEMP
+                                const tempProfileDesc: string[] = [
+                                    "very cold",
+                                    "cold",
+                                    "mild",
+                                    "warm",
+                                    "hot"
+                                ];
+                                const tempProfile = tempProfileDesc[temp];
+                                sptConfigsWeather.weather.temp = dbWeather.temp[temp];
+
+                                // PRESSURE
+                                const pressureProfileDesc: string[] = [
+                                    "very low pressure",
+                                    "low pressure",
+                                    "normal pressure",
+                                    "high pressure",
+                                    "very high pressure"
+                                ];
+                                const pressureProfile = pressureProfileDesc[pressure];
+                                sptConfigsWeather.weather.pressure = dbWeather.pressure[pressure];
+
+                                // SEASON
+                                let seasonOverrideValue: number;
+                                if (profileSeason === "spring") {
+                                    seasonOverrideValue = 3;
+                                } else if (profileSeason === "summer") {
+                                    seasonOverrideValue = 0;
+                                } else if (profileSeason === "autumn") {
+                                    seasonOverrideValue = 1;
+                                } else if (profileSeason === "winter") {
+                                    seasonOverrideValue = 2;
+                                }
+                                sptConfigsWeather.overrideSeason = seasonOverrideValue;
+
+                                // END UPDATE WEATHER AND SEASONS
+
+                                const newProfileData = {
+                                    mod: modName,
+                                    date: {
+                                        day: profileDay,
+                                        month: profileMonth,
+                                        year: profileYear
+                                    },
+                                    season: profileSeason,
+                                    weather: {
+                                        clouds: cloudsProfile,
+                                        rain: rainProfile,
+                                        wind: windProfile,
+                                        fog: fogProfile,
+                                        temp: tempProfile,
+                                        pressure: pressureProfile
+                                    }
+                                }
+
+                                const profilePath = path.join(modPath, "db/profile.json");
+                                fs.writeFileSync(profilePath, JSON.stringify(newProfileData, null, 4));
+
+                                // END UPDATE PROFILE
+                            }
+                        } catch (err) {
+                            this.logger.error(`Error in [${pkg.name}] /client/items: ${err.message}`);
+                        }
+                        return output;
+                    }
+                }
+            ],
+            `[${pkg.name}] /client/items`
+        );
+    }
+
+    public postSptLoad(container: DependencyContainer): void {
+        /*
+            LIGHT RAIN
+                clouds >= -0.699
+                wind <= 1.0
+                1.0 <= rain <= 3.0
+            RAIN
+                wind <= 1.0
+                rain > 3.0
+            CLEAR
+                clouds < -0.4
+                wind <= 1.0
+                rain < 2.0
+                fog <= 0.004
+            CLEAR WIND
+                clouds < -0.4
+                wind > 1.0
+                rain < 2.0
+                fog <= 0.004
+            PARTLY CLOUDY
+                -0.699 <= clouds <= -0.400
+                wind <= 1.0
+                rain < 2.0
+                fog <= 0.004
+            CLEAR FOG
+                clouds < -0.4
+                0.004 < fog < 0.100
+            FOG
+                fog >= 0.100
+            CLOUD FOG
+                -0.400 <= clouds <= 0.699
+                fog > 0.004
+            MOSTLY CLOUDY
+                -0.400 <= clouds <= 0.699
+                rain <= 1.0
+            FULL CLOUD
+                0.699 <= clouds <= 1.000
+                rain <= 1.0
+            THUNDER CLOUD
+                clouds >= 1.0
+                rain <= 1.0
+            CLOUD WIND
+                clouds >= 0.0
+                wind >= 2.0
+                rain <= 1.0
+            CLOUD WIND RAIN
+                wind >= 2.0
+                rain >= 2.0
+            WIND DIRECTION
+                East = 1,
+                North = 2,
+                West = 3,
+                South = 4,
+                SE = 5,
+                SW = 6,
+                NW = 7,
+                NE = 8,
+        */
+    }
+}
+
+module.exports = { mod: new DWSMain() };
